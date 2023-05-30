b894bafb9eae56690c1ec8ebf9b1ebbca5143228
UjvalC2P <ujval@c2p.group>
2023-05-30 17:25:55 +0530

    Add project files.

---
+++ HangFireDemo/Program.cs
@@ -0,0 +1,38 @@
+using Hangfire;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHangfire(x =>
+{
+    x.UseSimpleAssemblyNameTypeSerializer()
+    .UseRecommendedSerializerSettings()
+    .UseSqlServerStorage(builder.Configuration.GetConnectionString("DBConnection"));
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.UseHangfireDashboard();
+BackgroundJob.Enqueue(() => Console.WriteLine("Fire-and-forget Job Executed"));
+
+BackgroundJob.Schedule(() => Console.WriteLine("Delayed job executed"), TimeSpan.FromMinutes(1));
+
+RecurringJob.AddOrUpdate("easyjob", () => Console.WriteLine("Minutely Job executed"), Cron.Minutely);
+
+app.Run();
---
+++ HangFireDemo/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Hangfire;
+
+namespace HangFireDemo
+{
+    public static class Startup
+    {
+        public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            
+            services.AddHangfire(x =>
+            {
+                x.UseSqlServerStorage(configuration.GetConnectionString("DBConnection"));
+            });
+            services.AddHangfireServer();
+        }
+    }
+}
